1e0680bdbee045fd12aece4142092ba309386f5e
SammyJankins <neroragno@mail.ru>
2021-06-12 13:55:14 +0700

    edits before restart

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import json
 import os
 import time
-from pprint import pprint
 
 from bs4 import BeautifulSoup
 import requests
@@ -12,9 +11,9 @@ user = os.environ.get('TGUSER')
 
 file_path = 'cars.txt'
 
-URL = ('https://auto.ru/krasnoyarsk/cars/vendor-foreign/all/?top_days=3&year_from=2010&price_to=500000&km_age_to='
+URL = ('https://auto.ru/krasnoyarsk/cars/vendor-foreign/all/?top_days=3&year_from=2010&price_to=550000&km_age_to='
        '150000&displacement_to=2000&clearance_from=150&owners_count_group=LESS_THAN_TWO&transmission=ROBOT&'
-       'transmission=AUTOMATIC&transmission=VARIATOR&transmission=AUTO&sort=cr_date-desc')
+       'transmission=AUTOMATIC&transmission=VARIATOR&transmission=AUTO&sort=cr_date-desc&steering_wheel=LEFT')
 
 # owners options LESS_THAN_TWO / ONE
 
@@ -27,41 +26,52 @@ def check_cars():
     response.encoding = 'utf-8'
 
     soup = BeautifulSoup(response.text, features='html.parser')
-    cars = soup.find_all('div', {'class': 'ListingItem-module__container'}, limit=1)
+    cars = soup.find_all('div', {'class': 'ListingItem-module__container'}, limit=5)
     cars_dict = {
         tag_link[-2]:
             {
                 'model': model,
                 'link': tag.find('a', {'class': 'OfferThumb'}).attrs['href'],
             }
+
         for tag in cars
         if (tag_link := tag.find('a', {'class': 'OfferThumb'}).attrs['href'].split('/'),
             model := tag_link[-4],)
         if model in ['toyota', 'nissan', 'honda', 'volvo', 'kia', 'renault',
                      'chevrolet', 'mitsubishi', 'mazda', 'ford', 'audi',
-                     'hyundai', 'skoda', 'volkswagen']
+                     'hyundai', 'skoda', 'volkswagen', ]
     }
     return cars_dict
 
 
 def check_file():
+    timer = 960
+
     while True:
         try:
             with open(file_path, mode='r', encoding='utf-8') as f:
                 cars_in_file = json.load(f)
         except Exception:
             cars_in_file = dict()
+        if cars_in_file is None:
+            cars_in_file = dict()
         checked = check_cars()
-        pprint(checked)
-        pprint(cars_in_file)
+
         if result := checked.keys() - cars_in_file.keys():
             car = result.pop()
             with open(file_path, mode='w', encoding='utf-8') as f:
-                f.write(json.dumps({car: checked[car]}))
+                cars_in_file.update({car: checked[car]})
+                f.write(json.dumps(cars_in_file))
             bot.send_message(chat_id=user, text=checked[car]['link'])
-            print(checked[car]['link'])
         else:
             print('no')
+            if timer == 0:
+                bot.send_message(chat_id=user, text='No new cars in last 8 hours')
+                timer = 960
+            else:
+                timer -= 1
+            print(timer)
+
         time.sleep(30)
 
 
